245c6009518fda0679be6842dc01e90e6741c180
Anastasia Haller <anadana87@gmail.com>
2023-09-15 17:03:13 +0200

    implement get HTTP request

--- src/App.js
+++ src/App.js
@@ -2,39 +2,48 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import StatusBarMessage from './components/UI/StatusBarMessage';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, getCartData } from './store/cart-slice';
 
 let isInitialRunning = true;
 
 function App() {
-  const isCartVisible = useSelector((state) => state.main.isCarVisible);
+  const isCartVisible = useSelector((state) => state.main.isCartVisible);
   const cart = useSelector((state) => state.cart);
-  const statusMessage = useSelector((state) => state.main.statusMessage)
+  const statusMessage = useSelector((state) => state.main.statusMessage);
 
   const dispatchAction = useDispatch();
 
   useEffect(() => {
-    if(isInitialRunning) {
+    dispatchAction(getCartData());
+  }, []);
+
+  useEffect(() => {
+    if (isInitialRunning) {
       isInitialRunning = false;
       return;
     }
 
-    dispatchAction(sendCartData(cart));
-  },[cart]);
+    if (cart.isCartContentChanged) {
+      dispatchAction(sendCartData(cart));
+    }
+  }, [cart]);
 
   return (
-    <>
-      {statusMessage && <StatusBarMessage
-        status={statusMessage.status}
-        title={statusMessage.title}
-        message={statusMessage.message} />}
-    <Layout>
-      {isCartVisible && <Cart />}
-      <Products />
-    </Layout>
-    </>
+    <Fragment>
+      {statusMessage && (
+        <StatusBarMessage
+          status={statusMessage.status}
+          title={statusMessage.title}
+          message={statusMessage.message}
+        />
+      )}
+      <Layout>
+        {isCartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
